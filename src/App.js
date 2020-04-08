3f0237d950db1c9449896fd833858de754a43e8e
unknown <wandersonsilva768@gmail.com>
2020-04-08 07:23:19 -0300

    estrutura estável, ainda sem lógica implementada

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
 import React, { useState, useEffect } from 'react';
 
 function App () {
-  const baseGrid = [10, 15, 20];
+  const gridSize = [8, 12, 15];
+  const [level, setLevel] = useState(0);
   const [tiles, setTiles] = useState([]);
-  const [difficult, setDifficult] = useState(0);
 
   useEffect(() => {
-    document.querySelector('#gameContainer > .box').style.setProperty('--shape', baseGrid[difficult]);
-  }, [difficult]);
+    setVirginTiles();
+  }, [level]);
+
+  useEffect(() => {
+    setVirginTiles();
+  }, []); // happens once time when its loaded
+
+  function setVirginTiles () {
+    let currentGridSize = gridSize[level];
+
+    let placeholderTiles = Array.from({ length: currentGridSize ** 2 }, (_, index) => {
+      return ({ index });
+    });
+
+    setTiles(placeholderTiles);
+
+    setMutualCSSProperties(
+      document.querySelector('#gameContainer > .box'),
+      [
+        { propName: '--shape', propValue: currentGridSize },
+        { propName: 'font-size', propValue: `${29 - currentGridSize}px` },
+        { propName: 'line-height', propValue: `${[50, 33, 26][level]}px` }
+      ]
+    );
+  }
+
+  function random (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  function setMutualCSSProperties (element, props) {
+    for (let index = 0, len = props.length; index < len; index++) {
+      let current = props[index];
+
+      element.style.setProperty(current.propName, current.propValue);
+    }
+  }
 
   return (
     <div id="container">
       <div id="gameContainer">
         <div className="box">
           {
-            [...new Array(baseGrid[difficult] ** 2)].map(e => <div></div>)
+            tiles.map(e => <div key={e.index}> { random(1, 8) } </div>)
           }
         </div>
       </div>
@@ -28,7 +63,7 @@ function App () {
         <div className="gameControls">
           <div className="gameControlButton">Play</div>
           <div className="gameControlButton">Melhores Tempos</div>
-          <div className="gameControlButton" onClick={e => setDifficult(difficult + 1 > baseGrid.length - 1 ? 0 : difficult + 1)}>Alterar Dificuldade</div>
+          <div className="gameControlButton" onClick={e => setLevel(level + 1 > gridSize.length - 1 ? 0 : level + 1)}>Alterar Dificuldade</div>
         </div>
       </div>
     </div>
